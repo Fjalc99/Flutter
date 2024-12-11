98f16a24932eacb719bfee4fc59bb4f3f257d65b
Francisco José Alcantarilla Calado <alcantarilla.cafra24@triana.salesianos.edu>
2024-12-11 19:14:21 +0100

    Lista Start wars

--- starwhat/lib/screens/people_screen.dart
+++ starwhat/lib/screens/people_screen.dart
@@ -22,8 +22,19 @@ class _PeopleScreenState extends State<PeopleScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('People'),
+        backgroundColor: Color(Colors.black.value),
+        title: Row(
+          children: [
+            Image.network(
+              'https://img.icons8.com/color/512/star-wars.png', // Ruta de la imagen del logo
+              height: 60, // Ajusta la altura del logo
+            ),
+            const SizedBox(width: 10),
+            const Text('People', style: TextStyle(color: Colors.white)),
+          ],
+        ),
       ),
+      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
       body: FutureBuilder<PeopleResponse>(
         future: peopleResponse,
         builder: (context, snapshot) {
@@ -51,32 +62,50 @@ class _PeopleScreenState extends State<PeopleScreen> {
   }
 
   Widget _buildPeopleList(PeopleResponse peopleResponse) {
-    return ListView.builder(
-        itemCount: peopleResponse.results!.length,
-        itemBuilder: (context, index) {
-          return SizedBox(
-            child: Column(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image(
-                    image: NetworkImage('https://starwars-visualguide.com/assets/img/characters/${index + 1}.jpg'),
-                    width: 250,
+  return ListView.builder(
+    itemCount: peopleResponse.results!.length,
+    itemBuilder: (context, index) {
+      return Card(
+        margin: const EdgeInsets.all(10),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        color: const Color.fromARGB(255, 50, 50, 50),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: Image(
+                  image: NetworkImage('https://starwars-visualguide.com/assets/img/characters/${index + 1}.jpg'),
+                  width: 180,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 180,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Nombre: ${peopleResponse.results![index].name}", style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                      Text(
+                        "Altura: ${peopleResponse.results![index].height}", style: const TextStyle(color: Colors.white),
+                      ),
+                      Text("Genero: ${peopleResponse.results![index].gender}", style: const TextStyle(color: Colors.white),
+                      )
+                    ],
                   ),
                 ),
-                  SizedBox(
-                    width: 250,
-                    child: ListTile(
-                        
-                      title: Text(peopleResponse.results![index].name!),
-                      subtitle: Text(peopleResponse.results![index].gender!),
-                    ),
-                  ),
-                
-               
-              ],
-            ),
-          );
-        });
-  }
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
 }
