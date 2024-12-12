26feaf245a94fc7ab026efa25b2b30d763bc8d1c
alcantarilla.cafra24 <alcantarilla.cafra24@triana.salesianos.edu>
2024-12-12 12:32:56 +0100

    Lista Start Wars

--- starwhat/lib/screens/people_screen.dart
+++ starwhat/lib/screens/people_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:starwhat/models/people_response/people_response.dart';
 import 'package:http/http.dart' as http;
 
